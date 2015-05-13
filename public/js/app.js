4f2a890a06b2045d40189da9040ce2fb3675b0af
banzayche <dharm.serg@gmail.com>
2015-05-13 15:51:33 +0300

    Second Step full code of our app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,32 @@
 /*global Backbone */
 'use strict';
+
+var MyLibrarryApp = Marionette.Application.extend({
+	onStart: function(){
+		this.setRootLayout();
+		
+		this.GeneralCollection = new this.modelCollection.CollectionBook();
+		this.GeneralCollection
+			.fetch()
+			.done(function(){
+				Backbone.history.start({pushState: true});
+			});
+	},
+
+	setRootLayout: function(){
+		this.root = new this.staticViews.GeneralView();
+	}
+});
+
+window.myLibrarryApp = new MyLibrarryApp();
+
+(function(){
+	var filterState = new Backbone.Model({
+		filter: 'all',
+		list_type: 'table'
+	});
+
+	myLibrarryApp.reqres.setHandler('filterState', function(){
+		return filterState;
+	});
+})();
